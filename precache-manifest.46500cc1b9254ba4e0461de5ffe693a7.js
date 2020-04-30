befaf6061a7cda6aafaa6fd231a8da1556e712dd
Yanina Trekhleb <yanisavchenko@gmail.com>
2020-04-30 18:45:12 +0200

    Updates

---
+++ precache-manifest.46500cc1b9254ba4e0461de5ffe693a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec9cf9bdc9fe22695d0446a7261b0c5",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "73a58834f4403e8c5977",
+    "url": "/to-do-list/static/css/main.5cf976cc.chunk.css"
+  },
+  {
+    "revision": "053d046535df3072ae6c",
+    "url": "/to-do-list/static/js/2.b1389243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.b1389243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a58834f4403e8c5977",
+    "url": "/to-do-list/static/js/main.39566097.chunk.js"
+  },
+  {
+    "revision": "f9e60dd5e5d2cf63f3b7",
+    "url": "/to-do-list/static/js/runtime-main.34f8e458.js"
+  }
+]);
